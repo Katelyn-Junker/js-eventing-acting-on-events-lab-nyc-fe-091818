ae9ec4f3018c4d521a584067438eb94f1324831f
Katelyn Junker <kjunker@villanova.edu>
2018-10-30 23:53:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const dodger = document.getElementById('dodger')
 
-dodger.style.backgroundColor = 'pink'
+let dodgerPosition = 0
+
+dodger.style.left = '180px'
+
+function moveDodger(){
+  
+}
+
+dodger.addEventListener('keydown', moveDodger)
