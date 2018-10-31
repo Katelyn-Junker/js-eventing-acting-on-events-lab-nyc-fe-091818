089e7223fea8a729b4d485259664c8e3e5b29bf9
Katelyn Junker <kjunker@villanova.edu>
2018-10-31 00:03:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const dodger = document.getElementById('dodger')
 
 function moveDodger(event){
   const keycode = event.which
+  if (keycode=== 37){
+    // left
+  }else if (keycode===39){
+    // right
+  }else if (keycode ===38){
+    // up 
+  }else if (keycode ===40){
+    // down 
+  }
+
 }
 
 dodger.addEventListener('keydown', moveDodger)
