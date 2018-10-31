d7c17999e5e0c2f1e5dc89ca04d86e8fb0aaac2a
Katelyn Junker <kjunker@villanova.edu>
2018-10-31 00:30:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,19 +15,19 @@ function moveDodger(event){
     
     dodger.style.left = (oldLeft- 10)+ 'px'
     
-  }else if (keycode===39){
-    // right
-  }else if (keycode ===38){
-    // up 
-  }else if (keycode ===40){
-    // down 
+  } else if (keycode===39){
+    moveDodgerRight
   }
 
 }
 
 function moveDodgerRight(event){
   if (event.which=== 39){
+    let olfLocation = dodger.style.left
     
+    oldLocation= parseInt(oldLocation, 10)
+    
+    dodger.style.
   }
 }
 
